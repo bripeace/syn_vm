75f1e5688d919bcedb451f4e30740dd47cd97658
Brian Peace <bpeace@gmail.com>
2016-12-02 00:26:02 -0500

    noop, out, halt codes implemented

--- vm.c
+++ vm.c
@@ -1,11 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "inst.h"
 
 #define NUM_REG 8
-const int reg_count = NUM_REG;
+#define STACK_SIZE 1024
+//static int stack_size = STACK_SIZE;
+//static const int reg_count = NUM_REG;
 static int r[NUM_REG];
 static int *stack;
 static int *mem;
+static int run = 1;
 
 #define MAX_INT 32767
 #define WORD 2
@@ -19,6 +23,76 @@ void print_regs()
     }
 }
 
+void vm_push(int i)
+{
+
+}
+
+void setup_mem()
+{
+    int i = 0;
+    for(i = 0; i < 100; ++i)
+    {
+        stack[i] = noop;
+    }
+    stack[i] = out;
+    stack[i+1] = 'b';
+    stack[i+2] = out;
+    stack[i+3] = 'r';
+    stack[i+4] = out;
+    stack[i+5] = 'a';
+    stack[i+6] = out;
+    stack[i+7] = 'v';
+    stack[i+8] = out;
+    stack[i+9] = 'e';
+    stack[i+10] = out;
+    stack[i+11] = 's';
+    stack[i+12] = out;
+    stack[i+13] = '\n';
+}
+
+int fetch()
+{
+    static int *sp = NULL;
+
+    if (sp == NULL)
+    {
+        sp = stack;
+    }
+
+    int ret = *sp;
+    ++sp;
+    return ret;
+}
+
+void exec (instruction i)
+{
+    switch(i)
+    {
+        case halt:
+            run = 0;
+        break;
+        case noop:
+         //   printf("noop\n");
+        break;
+        case out:
+            printf("%c",fetch());
+        break;
+        default:
+        break;
+    }
+
+}
+
+void run_vm()
+{
+    while (run)
+    {
+        instruction inst = fetch();
+        exec(inst);
+    }
+
+}
 int main(int argc, const char **argv)
 {
 
@@ -29,5 +103,8 @@ int main(int argc, const char **argv)
     
     printf("Mem base: %p\n", (void *)mem);
     printf("Stack   : %p\n", (void *)stack);
+
+    setup_mem();
+    run_vm();
 }
 
